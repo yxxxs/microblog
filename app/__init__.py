9af22952c59f56028a01e105292c446888477d2c
李晨 <js.lichen@qq.com>
2019-01-27 14:47:30 +0800

    增加了数据库

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app)
-
+migrate = Migrate(app, db)
 
 from app import routes, models
--- config.py
+++ config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
