a5e70fd2dd2c43c2e7c01b149fe39b3466cb78de
李晨 <js.lichen@qq.com>
2019-02-02 11:26:02 +0800

    增加了用户个人信息，如头像，关于等

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,94 @@
-from flask import render_template, redirect, url_for
-from app import app
-from forms import LoginForm
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from forms import LoginForm, RegistrationForm, EditProfileForm
+from app.models import User
+
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'yxxxs'}
     posts = [
-        {'author': {'username': 'Zhoujie'},
-         'body': 'where r u'},
+        {'author': {'username': 'Zhoujie'}, 'body': 'where r u'},
 
-        {'author': {'username': 'Xiaobao'},
-         'body': 'da xue lu'}
+        {'author': {'username': 'Xiaobao'}, 'body': 'da xue lu'}
     ]
 
-    return render_template('index.html', title = 'home', user = user, posts = posts)
+    return render_template('index.html', title='home', posts=posts)
 
 
-@app.route('/login', methods = ['get', 'post'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).decode_netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html', title = 'sign in', form = form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('congratulations, you are now a registered user')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'test post 1'},
+        {'author': user, 'body': 'test post 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def befor_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('your change have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit profile', form=form)
+
--- forms.py
+++ forms.py
@@ -1,9 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('USERNAME', validators=[DataRequired()])
     password = PasswordField('PASSWORD', validators=[DataRequired()])
     remember_me = BooleanField('REMEMBER ME')
     submit = SubmitField('SIGH IN')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('please use different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('please use different email')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('submit')
